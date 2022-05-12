de99baeb471cab38adcda0edb0afd8d443bfa3ef
PreranaMalge <preranamalge123@gmail.com>
2022-05-13 00:00:25 +0530

    update

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,17 @@
 // import using require
 
+const { default: Shape } = require('./shape');
+
 // declare class
 
 
 // export class using module.exports
+
+ var Circle = class Circle extends Shape {
+    constructor(){
+        this.calculateArea() = function(){
+            console.log('calulate area');
+        };
+    }
+}
+module.exports.Circle = Circle ;
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,18 @@
 // declare class
 
 // export class using module.exports
+
+//Create a base class called "Shape" with methods drawShape, calculateArea and color property that is initialised with a color.
+
+import React, { Component } from "react";
+
+class Shape extends Component{
+    constructor(){
+        this.drawShape() = function(){};
+        this.calculateArea() = function(){};
+        this.color()="red";
+
+
+    }
+}
+export default Shape;
